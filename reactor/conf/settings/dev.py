c1a251696fec9e9fa304f4d8360bf405393eedef
Kamil Paduszyński <paduszyk@gmail.com>
2024-10-17 16:31:23 +0200

    Inspect ERD using django-schema-graph

--- reactor/conf/settings/dev.py
+++ reactor/conf/settings/dev.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+from schema_graph.views import Schema
+
+from django.urls import path
+
 from . import debug
 
 
@@ -9,3 +13,21 @@ class Settings(debug.Settings):
         super().pre_load()
 
         cls.env.read_env(Path.cwd() / ".env", overwrite=True)
+
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):
+        return [
+            *super().INSTALLED_APPS,
+            "schema_graph",
+        ]
+
+    # URLs
+
+    @classmethod
+    def get_urlpatterns(cls):
+        return [
+            *super().get_urlpatterns(),
+            path("schema-graph/", Schema.as_view()),
+        ]
