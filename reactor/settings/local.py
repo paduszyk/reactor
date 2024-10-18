6f5d6e0f398ff31a3e77c29da5c4c74dc1ab091e
Kamil Paduszyński <paduszyk@gmail.com>
2024-10-18 21:02:26 +0200

    Set up auxiliary development Django apps

--- reactor/settings/local.py
+++ reactor/settings/local.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+from debug_toolbar.toolbar import debug_toolbar_urls
+from schema_graph.views import Schema
+
+from django.urls import path
+
 from . import debug
 
 
@@ -11,3 +16,37 @@ class Settings(debug.Settings):
         super().pre_load()
 
         cls.env.read_env(Path.cwd() / ".env", overwrite=True)
+
+    # Security
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):
+        return [
+            *super().INSTALLED_APPS,
+            "debug_toolbar",
+            "schema_graph",
+            "xlsx_serializer",
+        ]
+
+    # Middleware
+
+    @property
+    def MIDDLEWARE(self):
+        return [
+            *super().MIDDLEWARE,
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+
+    @classmethod
+    def get_urlpatterns(cls):
+        return [
+            *super().get_urlpatterns(),
+            *debug_toolbar_urls(prefix="debug-toolbar/"),
+            path("schema-graph/", Schema.as_view()),
+        ]
