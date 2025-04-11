2bebcf0242f9f65a46303af571dae7ad3dfa1a15
Kamil Paduszyński <paduszyk@gmail.com>
2025-04-11 20:36:56 +0200

    Added django-schema-graph for enhanced schema development.

--- reactor/conf/settings/development.py
+++ reactor/conf/settings/development.py
@@ -18,5 +18,27 @@ class Settings(Common):
 
     SECRET_KEY = "django-insecure-secret-key"  # noqa: S105
 
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):
+        return [
+            *super().INSTALLED_APPS,
+            "schema_graph",
+        ]
+
+    # URLs
+
+    @classmethod
+    def get_urlpatterns(cls):
+        from schema_graph.views import Schema
+
+        from django.urls import path
+
+        return [
+            *super().get_urlpatterns(),
+            path("schema-graph/", view=Schema.as_view()),
+        ]
+
 
 Settings.load(__name__)
