db4fc32c69ac33440153a08b0ced59eb589f5797
Kamil Paduszyński <paduszyk@gmail.com>
2025-04-19 16:30:49 +0200

    feat: add debugging and dev tools for Django

--- reactor/conf/settings/dev.py
+++ reactor/conf/settings/dev.py
@@ -10,6 +10,45 @@ class Settings(Common):
 
     SECRET_KEY = "django-insecure-secret-key"  # noqa: S105
 
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):  # noqa: N802
+        return [
+            *super().INSTALLED_APPS,
+            "debug_toolbar",
+            "schema_graph",
+            "xlsx_serializer",
+        ]
+
+    # Middleware
+
+    @property
+    def MIDDLEWARE(self):  # noqa: N802
+        return [
+            *super().MIDDLEWARE,
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+
+    # URLs
+
+    @classmethod
+    def get_urlpatterns(cls):
+        import debug_toolbar
+        from schema_graph.views import Schema
+
+        from django.urls import include, path
+
+        return [
+            *super().get_urlpatterns(),
+            path("debug-toolbar/", include(debug_toolbar.urls)),
+            path("schema-graph/", view=Schema.as_view()),
+        ]
+
     # Storages
 
     STATIC_URL = "static/"
