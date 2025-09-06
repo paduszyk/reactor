1cc9cb3d8962f910fc647c8a0649dc6b2ee0af48
Kamil Paduszyński <paduszyk@gmail.com>
2025-09-06 17:51:21 +0200

    feat: bootstrap Django project with development settings

---
+++ reactor/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+]
