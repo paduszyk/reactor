17cd8e91caed29fc2cbce84da59919701ef2b7ab
Kamil Paduszyński <paduszyk@gmail.com>
2025-09-12 18:47:30 +0200

    Bootstrap Django + PostgreSQL project for development

---
+++ reactor/conf/settings/dev.py
@@ -0,0 +1,35 @@
+from environs import env
+
+from .common import CommonSettings
+
+
+class DevSettings(CommonSettings):
+    # Debugging
+
+    DEBUG = env.bool("DJANGO_DEBUG", default=True)
+
+    # Security
+
+    SECRET_KEY = env.str("DJANGO_SECRET_KEY", default="django-secret-key")
+
+    # Storages
+
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
+
+    MEDIA_URL = "media/"
+
+    MEDIA_ROOT = CommonSettings.BASE_DIR / "media"
+
+    STATIC_URL = "static/"
+
+    STATIC_ROOT = CommonSettings.BASE_DIR / "staticfiles"
+
+
+DevSettings.load()
