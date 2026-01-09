3f51c810545061923b0c3356bcfff01fbb9459ed
Kamil Paduszyński <paduszyk@gmail.com>
2026-01-09 18:10:14 +0100

    Integrate Django Debug Toolbar for enhanced development

--- reactor/conf/settings/dev.py
+++ reactor/conf/settings/dev.py
@@ -1,8 +1,54 @@
+from importlib import import_module
+
 import decouple
 import dj_database_url as db_url
 from decouple import config as env
 
+from .base import BasePlugin
 from .common import CommonSettings
+
+
+class DebugToolbarPlugin(BasePlugin):
+    @classmethod
+    def is_active(cls):
+        try:
+            import_module("debug_toolbar")
+        except ModuleNotFoundError:
+            return False
+
+        return env("DJANGO_DEBUG_TOOLBAR", cast=decouple.strtobool, default="true")
+
+    # Security
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):
+        return [
+            *super().INSTALLED_APPS,
+            "debug_toolbar",
+        ]
+
+    # Middleware
+
+    @property
+    def MIDDLEWARE(self):
+        return [
+            *super().MIDDLEWARE,
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+
+    # URLs
+
+    @classmethod
+    def get_urlpatterns(cls):
+        from debug_toolbar.toolbar import debug_toolbar_urls
+
+        return super().get_urlpatterns() + debug_toolbar_urls()
 
 
 class DevSettings(CommonSettings):
