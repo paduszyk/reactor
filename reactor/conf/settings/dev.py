bc969212e9d6997890416b3deeac656aae8c5833
Kamil Paduszyński <paduszyk@gmail.com>
2026-06-24 13:48:16 +0200

    chore: add Django Debug Toolbar extension to dev settings

--- reactor/conf/settings/dev.py
+++ reactor/conf/settings/dev.py
@@ -1,8 +1,54 @@
+from importlib import import_module
+
 from environs import EnvError, env
 
+from .base import BaseExtension
 from .common import CommonSettings
 
 env.read_env()
+
+
+class DebugToolbarExtension(BaseExtension):
+    @classmethod
+    def is_enabled(cls):
+        try:
+            import_module("debug_toolbar")
+        except ModuleNotFoundError:
+            return False
+
+        return env.bool("DJANGO_DEBUG_TOOLBAR", default=True)
+
+    # Security
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+    # Apps
+
+    @property
+    def INSTALLED_APPS(self):
+        return [
+            *super().INSTALLED_APPS,
+            "debug_toolbar",
+        ]
+
+    # Middleware
+
+    @property
+    def MIDDLEWARE(self):
+        return [
+            *super().MIDDLEWARE,
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+
+    # URLs
+
+    @classmethod
+    def get_urlpatterns(cls):
+        from debug_toolbar.toolbar import debug_toolbar_urls
+
+        return super().get_urlpatterns() + debug_toolbar_urls()
 
 
 class DevSettings(CommonSettings):
